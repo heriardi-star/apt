89ebfd2d57f1ba197b8ea595e800908586d4f432
Heri Ardiyanto <heriardi@mail.unnes.ac.id>
2026-02-23 17:58:50 +0700

    rev icon

--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>APT UNNES 2026</title>
-    <link rel="icon" type="image/png" href="picture/unnes.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="picture/unnes.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 
     <style>
