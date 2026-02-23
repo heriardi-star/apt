684d13cf6dbb477e7fd68f2822cb97acc396d27d
Heri Ardiyanto <heriardi@mail.unnes.ac.id>
2026-02-23 17:48:37 +0700

    beri icon

--- application/views/index.php
+++ application/views/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>APT UNNES 2026</title>
+    <link rel="shortcut icon" href="picture/unnes.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 
     <style>
